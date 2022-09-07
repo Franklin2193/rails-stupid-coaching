b05896055017b0c147d5d66151557f3407c4a3fd
franklin duque <franklinmont@gmail.com>
2022-09-06 23:05:10 -0500

    Ejercicio previo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,19 @@
 class PagesController < ApplicationController
   def home
   end
+
   def about
   end
 
   def contact
+    @members = ["claire", "toni", "santi", "sarah"]
+
+    search = params[:member] # what they typed in the form
+
+    if search
+      @members = @members.select do |member|
+      member == search.downcase
+      end 
+    end
   end
 end
