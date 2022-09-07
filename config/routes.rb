a0e5babbb83c1691f0658840e32e836872a7e99c
franklin duque <franklinmont@gmail.com>
2022-09-06 22:12:08 -0500

    Ejercicio

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # Get localhost:3000/about
+  root to: "pages#home"
+  get "/about", to: "pages#about"
+  # verb "/path", to: "controller#action"
+  get "/contact", to: "pages#contact"
 end
