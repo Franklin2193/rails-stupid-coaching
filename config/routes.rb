c96dd3c116f6840e3196aeb60adbe7303341cdc2
franklin duque <franklinmont@gmail.com>
2022-09-08 21:06:54 -0500

    Ejercicio terminado

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
   # Get localhost:3000/about
-  root to: "pages#home"
-  get "/about", to: "pages#about"
-  # verb "/path", to: "controller#action"
-  get "/contact", to: "pages#contact"
+  get 'answer', to: 'questions#answer'
+  get 'ask', to: 'questions#ask'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
